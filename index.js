cc74aa1f41b3be81dc20883cdb316cd0438b38b2
ubuntu <vijayreddi@live.in>
2018-04-23 18:07:15 +0530

    implemented socket.io

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var express = require("express");
+var express = require("express.io");
 var app = express();
+app.http().io();
+var port = 3000;
 
-console.log("server started");
+console.log("server started on port: " + port);
 
 app.use(express.static(__dirname + "/public"));
 
@@ -9,4 +11,21 @@ app.get("/", function(req, res){
 	res.render("index.ejs");
 });
 
-app.listen(3000);
+app.io.route('ready', function(req){
+	req.io.join(req.data);
+	app.io.room(req.data).broadcast('announce',{
+		message: 'New Client in the ' + req.data + ' room.'
+	})
+})
+
+app.io.route('send', function(req){
+	app.io.room(req.data.room).broadcast('message', {
+		message: req.data.message,
+		author: req.data.author
+	});
+})
+
+
+
+
+app.listen(port);
